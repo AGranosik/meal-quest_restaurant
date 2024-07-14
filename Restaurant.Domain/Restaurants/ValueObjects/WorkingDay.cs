d04ada3ad91e10996f62e7ffc747e5ddf989d912
AGranosik <adrian.granosik@gmail.com>
2024-07-14 11:56:28 +0200

    working day

---
+++ Restaurant.Domain/Restaurants/ValueObjects/WorkingDay.cs
@@ -0,0 +1,56 @@
+﻿using domain.Common.BaseTypes;
+using FluentResults;
+
+namespace domain.Restaurants.ValueObjects
+{
+    public class WorkingDay : ValueObject<WorkingDay>
+    {
+
+        public static Result<WorkingDay> Create(DayOfWeek day, TimeOnly from, TimeOnly to)
+        {
+            var validatioNResult = Validation(from, to);
+            if (validatioNResult.IsFailed)
+                return validatioNResult;
+
+            return new WorkingDay(day, from, to);
+        }
+
+        protected WorkingDay() { }
+        protected WorkingDay(DayOfWeek day, TimeOnly from, TimeOnly to)
+        {
+            Day = day;
+            From = from;
+            To = to;
+        }
+
+        public DayOfWeek Day { get; }
+        public TimeOnly From { get; }
+        public TimeOnly To { get; }
+
+        private static Result Validation(TimeOnly from, TimeOnly to)
+        {
+            if (from.Microsecond != 0 || to.Millisecond != 0)
+                return Result.Fail("Specify time without microseconds.");
+
+            if (from.Millisecond != 0 || to.Millisecond != 0)
+                return Result.Fail("Specify time without ms.");
+
+            if (from.Second != 0 || to.Second != 0)
+                return Result.Fail("Specify time without ms.");
+
+            if (from >= to)
+                return Result.Fail("From cannot be greater than to.");
+
+            return Result.Ok();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            WorkingDay? other = obj as WorkingDay;
+            return Day == other.Day && other.From == From && other.To == To;
+        }
+    }
+}
---
+++ Restaurant.UnitTests/Restaurants/ValueObjects/WorkingDayTests.cs
@@ -0,0 +1,160 @@
+﻿using domain.Restaurants.ValueObjects;
+using FluentAssertions;
+
+namespace unitTests.Restaurants.ValueObjects
+{
+    [TestFixture]
+    public class WorkingDayTests
+    {
+        [Test]
+        public void Creation_FromEqualTo_Failure()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(12, 00));
+            creationResult.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Creation_FromGeaterThanTo_Failure()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(13, 00), new TimeOnly(12, 00));
+            creationResult.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Creation_ToGreaterThanFrom_Success()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(13, 00), new TimeOnly(13, 01));
+            creationResult.IsSuccess.Should().BeTrue();
+        }
+
+        [Test]
+        [TestCase(DayOfWeek.Monday)]
+        [TestCase(DayOfWeek.Tuesday)]
+        [TestCase(DayOfWeek.Wednesday)]
+        [TestCase(DayOfWeek.Thursday)]
+        [TestCase(DayOfWeek.Friday)]
+        [TestCase(DayOfWeek.Saturday)]
+        [TestCase(DayOfWeek.Sunday)]
+        public void Creation_AllDayOfWeek_Success(DayOfWeek dayOfWeek)
+        {
+            var creationResult = WorkingDay.Create(dayOfWeek, new TimeOnly(13, 00), new TimeOnly(13, 01));
+            creationResult.IsSuccess.Should().BeTrue();
+        }
+
+        [Test]
+        public void Creation_FromCannotHaveMicroSeconds_Failuere()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00, 12, 1, 12), new TimeOnly(13, 01));
+            creationResult.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Creation_FromCannotHaveMiliSeconds_Failuere()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00, 12, 1), new TimeOnly(13, 01));
+            creationResult.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Creation_FromCannotHaveSeconds_Failuere()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00, 12), new TimeOnly(13, 01, 12));
+            creationResult.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Creation_ToCannotHaveMicroSeconds_Failuere()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(13, 00, 12, 1, 12));
+            creationResult.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Creation_ToCannotHaveMiliSeconds_Failuere()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(13, 00, 12, 1));
+            creationResult.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Creation_ToCannotHaveSeconds_Failuere()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(13, 00, 12));
+            creationResult.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Equality_SameReferenceTrue()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(13, 01));
+            var openingHours = creationResult.Value;
+            (openingHours == openingHours).Should().BeTrue();
+        }
+
+        [Test]
+        public void Equality_SameReference_True()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(13, 01));
+            var creationResult2 = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(13, 01));
+            var openingHours = creationResult.Value;
+            var openingHours2 = creationResult2.Value;
+
+            (openingHours == openingHours2).Should().BeTrue();
+        }
+
+        [Test]
+        public void Equality_DifferentFrom_False()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(13, 01));
+            var creationResult2 = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 01), new TimeOnly(13, 01));
+            var openingHours = creationResult.Value;
+            var openingHours2 = creationResult2.Value;
+
+            (openingHours == openingHours2).Should().BeFalse();
+        }
+
+        [Test]
+        public void Equality_DifferentTo_False()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(13, 01));
+            var creationResult2 = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 00), new TimeOnly(13, 11));
+            var openingHours = creationResult.Value;
+            var openingHours2 = creationResult2.Value;
+
+            (openingHours == openingHours2).Should().BeFalse();
+        }
+
+        [Test]
+        public void Equality_DifferentBoth_False()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 01), new TimeOnly(13, 01));
+            var creationResult2 = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 0), new TimeOnly(13, 11));
+            var openingHours = creationResult.Value;
+            var openingHours2 = creationResult2.Value;
+
+            (openingHours == openingHours2).Should().BeFalse();
+        }
+
+        [Test]
+        public void Equality_DifferentDay_True()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 01), new TimeOnly(13, 01));
+            var creationResult2 = WorkingDay.Create(DayOfWeek.Thursday, new TimeOnly(12, 01), new TimeOnly(13, 01));
+            var openingHours = creationResult.Value;
+            var openingHours2 = creationResult2.Value;
+
+            (openingHours == openingHours2).Should().BeFalse();
+        }
+
+        [Test]
+        public void Equality_SameValues_True()
+        {
+            var creationResult = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 01), new TimeOnly(13, 01));
+            var creationResult2 = WorkingDay.Create(DayOfWeek.Monday, new TimeOnly(12, 01), new TimeOnly(13, 01));
+            var openingHours = creationResult.Value;
+            var openingHours2 = creationResult2.Value;
+
+            (openingHours == openingHours2).Should().BeTrue();
+        }
+    }
+}
