909400742ad5c50d8d379643de1c71b9b554edc9
AGranosik <adrian.granosik@gmail.com>
2024-07-21 13:01:35 +0200

    create restaurant command validation

--- Restaurant.UnitTests/Application/CreateRestaurantCommandHandlerTests.cs
+++ Restaurant.UnitTests/Application/CreateRestaurantCommandHandlerTests.cs
@@ -38,6 +38,105 @@ namespace unitTests.Application
             result.IsFailed.Should().BeTrue();
         }
 
+        [Test]
+        public async Task Command_OwnerCannotBeNull_Fail()
+        {
+            var handler = CreateHandler();
+            var command = new CreateRestaurantCommand(null, null);
+            var result = await handler.Handle(command, CancellationToken.None);
+            
+            result.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Command_OpeningHorusCannotBeNull_Fail()
+        {
+            var handler = CreateHandler();
+            var command = new CreateRestaurantCommand(new CreateOwnerCommand(null, null, null), null);
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public async Task Command_OwnerNameCannotBenullOrEmpty_Fail(string? ownerName)
+        {
+            var owner = new CreateOwnerCommand(ownerName, null, null);
+            var handler = CreateHandler();
+            var command = new CreateRestaurantCommand(owner, new OpeningHoursCommand(null!));
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.IsFailed.Should().BeTrue();
+        }
+
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public async Task Command_OwnerSurnameCannotBenullOrEmpty_Fail(string? ownerSurname)
+        {
+            var owner = new CreateOwnerCommand("name", ownerSurname, null);
+            var handler = CreateHandler();
+            var command = new CreateRestaurantCommand(owner, new OpeningHoursCommand(null!));
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Command_OwnerAddressCannotBenull_Fail()
+        {
+            var owner = new CreateOwnerCommand("name", "surname", null);
+            var handler = CreateHandler();
+            var command = new CreateRestaurantCommand(owner, new OpeningHoursCommand(null!));
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public async Task Command_OwnerSteetCannotBeNullOrEmpty_Fail(string? ownerSteet)
+        {
+            var address = new CreateAddressCommand(ownerSteet, null, 0, 0);
+            var owner = new CreateOwnerCommand("name", "surname", address);
+            var handler = CreateHandler();
+            var command = new CreateRestaurantCommand(owner, new OpeningHoursCommand(null!));
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public async Task Command_CityNameCannotBeNullOrEmpty_Fail(string? ownerCity)
+        {
+            var address = new CreateAddressCommand("street", ownerCity, 0, 0);
+            var owner = new CreateOwnerCommand("name", "surname", address);
+            var handler = CreateHandler();
+            var command = new CreateRestaurantCommand(owner, new OpeningHoursCommand(null!));
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.IsFailed.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Command_WorkingDaysCanntBeEmpty_Fail()
+        {
+            var openingHours = new OpeningHoursCommand([]);
+            var address = new CreateAddressCommand("street", "City", 0, 0);
+            var owner = new CreateOwnerCommand("name", "surname", address);
+            var handler = CreateHandler();
+            var command = new CreateRestaurantCommand(owner, openingHours);
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            result.IsFailed.Should().BeTrue();
+        }
+
         private CreateRestaurantCommandHandler CreateHandler()
             => new(_repositoryMock.Object);
     }
--- application/Restaurants/Commands/CreateRestaurantCommand.cs
+++ application/Restaurants/Commands/CreateRestaurantCommand.cs
@@ -1,4 +1,5 @@
-﻿using domain.Restaurants.ValueObjects.Identifiers;
+﻿using domain.Restaurants.Aggregates;
+using domain.Restaurants.ValueObjects.Identifiers;
 using FluentResults;
 using infrastructure.Database.RestaurantContext.Repositories;
 using MediatR;
@@ -16,20 +17,54 @@ namespace application.Restaurants.Commands
             if (validationResult.IsFailed)
                 return validationResult;
 
-            return Result.Fail("s");
+            var restaurant = Restaurant.Create()
+
+            return Result.Ok();
         }
 
-        private Result CommandValidation(CreateRestaurantCommand command)
+        private static Result CommandValidation(CreateRestaurantCommand command)
         {
             if (command is null)
                 return Result.Fail("Command cannot be null.");
+
+            if (command.Owner is null)
+                return Result.Fail("Owner cannot be null.");
+
+            if (command.OpeningHours is null)
+                return Result.Fail("Opening hours cannot be null.");
+            var owner = command.Owner;
+
+            if (string.IsNullOrEmpty(owner.Name))
+                return Result.Fail("Owners name cannot be empty.");
+
+            if (string.IsNullOrEmpty(owner.Surname))
+                return Result.Fail("Owners surname cannot be empty.");
+
+            var ownerAddress = owner.Address;
+
+            if (ownerAddress is null)
+                return Result.Fail("Owner address cannot be null.");
+
+            if (string.IsNullOrEmpty(ownerAddress.Street))
+                return Result.Fail("Owner street cannot be null.");
+
+            if (string.IsNullOrEmpty(ownerAddress.City))
+                return Result.Fail("Owner city cannot be null.");
+
+            var openingHours = command.OpeningHours;
+
+            if (openingHours is null)
+                return Result.Fail("Opening hours cannot be null.");
+
+            if (openingHours.WorkingDays.Count == 0)
+                return Result.Fail("Working days cannot be empty.");
 
             return Result.Ok();
         }
     }
 
 
-    public record CreateRestaurantCommand(CreateOwnerCommand? Owner, OpeningHoursCommand OpeningHours) : IRequest<Result<RestaurantId>>;
+    public record CreateRestaurantCommand(CreateOwnerCommand? Owner, OpeningHoursCommand? OpeningHours) : IRequest<Result<RestaurantId>>;
 
     public record CreateOwnerCommand(string? Name, string? Surname, CreateAddressCommand? Address);
 
