52dc0afbfd26b0675b1fe97ef20c65e5edd2523d
AGranosik <adrian.granosik@gmail.com>
2024-07-25 13:44:44 +0200

    begin with itegration tests

---
+++ integrationTests/ApiWebApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Testcontainers;
+using Npgsql;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Builders;
+using Testcontainers.PostgreSql;
+
+namespace integrationTests
+{
+    public class ApiWebApplicationFactory
+    {
+        
+        protected override async Task ConfigureWebHost(IWebHostBuilder builder)
+        {
+
+
+
+            builder.
+        }
+    }
+}
---
+++ integrationTests/BaseContainerIntegratioNTests.cs
@@ -0,0 +1,22 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Testcontainers.PostgreSql;
+
+namespace integrationTests
+{
+    public class BaseContainerIntegratioNTests
+    {
+        public static IContainer _postgresContainer;
+
+        public BaseContainerIntegrationTests()
+        {
+            _postgresContainer = new PostgreSqlBuilder()
+                .WithImage("postgres:14-alpine")
+                .WithUsername("admin")
+                .WithPassword("S3cret")
+                .WithPortBinding(5432)
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5432))
+                .Build();
+        }
+    }
+}
