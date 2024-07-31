27d0fd812a09814056cf18122181399b273631ba
AGranosik <adrian.granosik@gmail.com>
2024-07-31 15:22:21 +0200

    restaurant data faker

--- integrationTests/Restaurants/RestaurantCreateEndpointTestes.cs
+++ integrationTests/Restaurants/RestaurantCreateEndpointTestes.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 using FluentAssertions;
+using integrationTests.Restaurants.DataMocks;
 using Microsoft.EntityFrameworkCore;
 using webapi.Controllers.Restaurants.Requests;
 
@@ -22,19 +23,7 @@ namespace integrationTests.Restaurants
         [Test]
         public async Task CreateRestaurant_Valid_Created()
         {
-            var openingHours = new OpeningHoursRequest(
-            [
-                new(DayOfWeek.Monday, new DateTime(12, 12, 12, 11, 00, 00),new DateTime(12, 12, 12, 13, 00, 00)),
-                new(DayOfWeek.Tuesday, new DateTime(12, 12, 12, 11, 00, 00),new DateTime(12, 12, 12, 13, 00, 00)),
-                new(DayOfWeek.Wednesday, new DateTime(12, 12, 12, 11, 00, 00),new DateTime(12, 12, 12, 13, 00, 00)),
-                new(DayOfWeek.Thursday, new DateTime(12, 12, 12, 11, 00, 00),new DateTime(12, 12, 12, 13, 00, 00)),
-                new(DayOfWeek.Friday, new DateTime(12, 12, 12, 11, 00, 00),new DateTime(12, 12, 12, 13, 00, 00)),
-                new(DayOfWeek.Saturday, new DateTime(12, 12, 12, 11, 00, 00),new DateTime(12, 12, 12, 13, 00, 00)),
-                new(DayOfWeek.Sunday, new DateTime(12, 12, 12, 11, 00, 00),new DateTime(12, 12, 12, 13, 00, 00)),
-            ]);
-            var address = new CreateAddressRequest("street", "City", 0, 0);
-            var owner = new CreateOwnerRequest("name", "surname", address);
-            var request = new CreateRestaurantRequest(owner, openingHours);
+            var request = RestaurantDataFaker.ValidRequest();
 
             var response = await _client.PostAsJsonAsync("/api/Restaurant", request, CancellationToken.None);
             response.StatusCode.Should().Be(HttpStatusCode.OK);
